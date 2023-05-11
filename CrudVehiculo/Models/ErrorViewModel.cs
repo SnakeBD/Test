c6be49f3c51cc9712f243e48401ca5b866dc05a7
SnakeBD <sbd_mossuto@hotmail.com>
2023-05-11 18:22:24 -0400

    agregado proyecto

---
+++ CrudVehiculo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CrudVehiculo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ CrudVehiculo/Models/VehiculoModel.cs
@@ -0,0 +1,13 @@
+﻿namespace CrudVehiculo.Models
+{
+    public class VehiculoModel
+    {
+        public int Id { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public string Color { get; set; }
+        public string Patente { get; set; }
+        public int Anio { get; set; }
+        public int Cilindrada { get; set;}
+    }
+}
